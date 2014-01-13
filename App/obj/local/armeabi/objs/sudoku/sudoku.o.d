61fe70fa34f337cbc76aba7b8c623124c70d2acf
Jeff Hillman <android.hillman@gmail.com>
2014-01-13 13:33:42 -0700

    Removed extra lib directory

--- App/obj/local/armeabi/objs/sudoku/sudoku.o.d
+++ App/obj/local/armeabi/objs/sudoku/sudoku.o.d
@@ -1,26 +1,26 @@
 obj/local/armeabi/objs/sudoku/sudoku.o: jni/sudoku.c \
- /home/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/jni.h \
- /home/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
- /home/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
- /home/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/android/api-level.h \
- /home/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/string.h \
- /home/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/malloc.h \
- /home/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/android/log.h \
+ /Users/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/jni.h \
+ /Users/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/android/log.h \
  jni/sudoku.h jni/list.h
 
-/home/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/jni.h:
+/Users/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/jni.h:
 
-/home/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+/Users/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
 
-/home/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+/Users/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
 
-/home/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+/Users/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/android/api-level.h:
 
-/home/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/string.h:
+/Users/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/string.h:
 
-/home/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/malloc.h:
+/Users/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/malloc.h:
 
-/home/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/android/log.h:
+/Users/jeff/android-ndk/platforms/android-3/arch-arm/usr/include/android/log.h:
 
 jni/sudoku.h:
 
